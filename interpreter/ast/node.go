edf0374f5f876eb12d7800b0cbf37da0fdb0da1f
luke-goddard <l-goddard@protonmail.com>
2024-10-06 13:39:12 +0100

    fix formatting

--- interpreter/ast/node.go
+++ interpreter/ast/node.go
@@ -5,16 +5,16 @@ type Line int     // Line represents a line in the source code.
 type NodeType int // NodeType represents the type of a node. e.g BinaryExpression, Literal, Command, etc.
 
 const (
-	NodeTypeProgram           NodeType = iota // NodeTypeProgram represents a program. e.g add "buy dog" priority:High
-	NodeTypeCommand                           // NodeTypeCommand represents a command. e.g add "buy dog" priority:High
-	NodeTypeOption                            // NodeTypeOption represents an option. e.g priority:High
-	NodeTypeParam                             // NodeTypeParam represents a param. e.g "buy dog"
-	NodeTypeTag                               // NodeTypeTag represents a tag. e.g +tag
-	NodeTypeLiteral                           // NodeTypeLiteral represents a literal. e.g "buy dog"
-  NodeTypeExpressionStatement               // NodeTypeExpressionStatement represents an expression statement. e.g 1 + 2
-	NodeTypeLogicalExpression                 // NodeTypeLogicalExpression represents a logical expression. e.g 1 and 2
-	NodeTypeBinaryExpression                  // NodeTypeBinaryExpression represents a binary expression. e.g 1 + 2
-	NodeTypePair                              // NodeTypePair represents a pair. e.g priority:High
+	NodeTypeBinaryExpression  NodeType = iota               // NodeTypeBinaryExpression represents a binary expression. e.g 1 + 2
+	NodeTypeCommand                             // NodeTypeCommand represents a command. e.g add "buy dog" priority:High
+	NodeTypeExpressionStatement                 // NodeTypeExpressionStatement represents an expression statement. e.g 1 + 2
+	NodeTypeLiteral                             // NodeTypeLiteral represents a literal. e.g "buy dog"
+	NodeTypeLogicalExpression                   // NodeTypeLogicalExpression represents a logical expression. e.g 1 and 2
+	NodeTypeOption                              // NodeTypeOption represents an option. e.g priority:High
+	NodeTypePair                                // NodeTypePair represents a pair. e.g priority:High
+	NodeTypeParam                               // NodeTypeParam represents a param. e.g "buy dog"
+	NodeTypeProgram             // NodeTypeProgram represents a program. e.g add "buy dog" priority:High
+	NodeTypeTag                                 // NodeTypeTag represents a tag. e.g +tag
 )
 
 // Node represents a node in the AST.
