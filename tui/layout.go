fc8970dbd5571ac93fd3bb55d9ddff456b8f0d2d
luke-goddard <l-goddard@protonmail.com>
2024-10-14 08:45:55 +0100

    refactor tabs into seperate component

--- tui/layout.go
+++ tui/layout.go
@@ -9,22 +9,12 @@ import (
 	"github.com/luke-goddard/taskninja/tui/utils"
 )
 
-var (
-	inactiveTabBorder = tabBorderWithBottom("┴", "─", "┴")
-	activeTabBorder   = tabBorderWithBottom("┘", " ", "└")
-	docStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
-	highlightColor    = lipgloss.AdaptiveColor{Light: "#874BFD", Dark: "#7D56F4"}
-	inactiveTabStyle  = lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(highlightColor).Padding(0, 1)
-	activeTabStyle    = inactiveTabStyle.Border(activeTabBorder, true)
-	windowStyle       = lipgloss.NewStyle().BorderForeground(highlightColor).Padding(2, 0).Align(lipgloss.Center).Border(lipgloss.NormalBorder()).UnsetBorderTop()
-)
-
 var baseStyle = lipgloss.NewStyle().
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("240"))
 
 type model struct {
-	tabs       []string
+	tabs       *components.Tabs
 	table      *components.TaskTable
 	dimensions *utils.TerminalDimensions
 	activeTab  int
@@ -37,46 +27,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "q", "ctrl+c":
 			return m, tea.Quit
-		case "right", "l", "n", "tab":
-			m.activeTab = min(m.activeTab+1, len(m.tabs)-1)
-			return m, nil
-		case "left", "h", "p", "shift+tab":
-			m.activeTab = max(m.activeTab-1, 0)
-			return m, nil
 		}
 	}
 	var newTable, _ = m.table.Update(msg)
 	m.table = newTable
+
+	var newTabs, _ = m.tabs.Update(msg)
+	m.tabs = newTabs
+
 	return m, cmd
 }
 
 func (m model) View() string {
 	var document strings.Builder
-	var renderedTabs []string
-	for i, tab := range m.tabs {
-		var style lipgloss.Style
-		isFirst, isLast, isActive := i == 0, i == len(m.tabs)-1, i == m.activeTab
-		if isActive {
-			style = activeTabStyle
-		} else {
-			style = inactiveTabStyle
-		}
-		border, _, _, _, _ := style.GetBorder()
-		if isFirst && isActive {
-			border.BottomLeft = "│"
-		} else if isFirst && !isActive {
-			border.BottomLeft = "├"
-		} else if isLast && isActive {
-			border.BottomRight = "│"
-		} else if isLast && !isActive {
-			border.BottomRight = "┤"
-		}
-		style = style.Border(border)
-		renderedTabs = append(renderedTabs, style.Render(tab))
-	}
-	document.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...))
-	document.WriteString("\n")
-	// document.WriteString(baseStyle.Render(m.table.View()) + "\n")
+
+	// document.WriteString(lipgloss.JoinHorizontal(lipgloss.Top, renderedTabs...))
+	// document.WriteString("\n")
+	document.WriteString(m.tabs.View() + "\n")
 	document.WriteString(m.table.View() + "\n")
 	document.WriteString(m.table.HelpView() + "\n")
 	return document.String()
@@ -93,7 +60,7 @@ func NewTui() error {
 
 	var theme = utils.NewTheme()
 
-	var tabs = []string{"Tasks", "Projects", "Tags", "Settings"}
+	var tabs = components.NewTabs()
 	var model = model{
 		table:      components.NewTaskTable(baseStyle, dimensions, theme),
 		tabs:       tabs,
@@ -101,12 +68,4 @@ func NewTui() error {
 	}
 	tea.NewProgram(model, tea.WithAltScreen()).Run()
 	return nil
-}
-
-func tabBorderWithBottom(left, middle, right string) lipgloss.Border {
-	border := lipgloss.RoundedBorder()
-	border.BottomLeft = left
-	border.Bottom = middle
-	border.BottomRight = right
-	return border
 }
